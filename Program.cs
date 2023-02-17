19619be3062640e2a2446eb7634543d5180588bd
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-17 23:22:06 +0530

    Create object and call method

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@
         {
             Console.WriteLine("Welcome to Line Comparison Problems");
 
-            LenghtCalculation lenghtCalculation = new LenghtCalculation();
-            lenghtCalculation.lenghtCalculation();
+            CheckEquality checkEquality = new CheckEquality();
+            checkEquality.checkEquality();
         }
     }
 }
